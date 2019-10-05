cf8177024cdffdd33fdeb4cf3abaeb2d972ab6ec
binlee <sleticalboy@gmail.com>
2019-10-05 22:19:14 +0800

    variate, data type and function practise

---
+++ src/function.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 调用函数
+    anotherFunction("Hello Function")
+
+    length, area := multiRets(4, 5)
+    // length, area := multiRets(4, 5)
+    fmt.Println("length:", length, ", area:", area)
+
+    // 若要丢弃某返回值
+    anotherLength, _ := multiRets2(3, 9)
+    fmt.Println("another length:", anotherLength)
+}
+func anotherFunction(param string) {
+    fmt.Println("another function was called with:", param)
+}
+
+// 多返回值函数: 方式一
+// 返回矩形的周长和面积
+func multiRets(width int, height int) (int, int) {
+    return (width + height) * 2, width * height
+}
+
+// 多返回值函数：方式一
+// 返回矩形的周长和面积
+func multiRets2(width int, height int) (length, area int) {
+    length = (width + height) * 2
+    area = width * height
+    return
+}
