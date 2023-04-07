f0b54b126134dd42218646e293c792d503142bea
bin.li <sleticalboy@gmail.com>
2023-04-07 15:03:11 +0800

    feat: struct and class

--- src/basic/struct.go
+++ src/basic/struct.go
@@ -6,10 +6,23 @@ import "fmt"
 type Person struct {
 	// 命名字段
 	// 同类型的字段放在一行是为了紧凑
-	name string
+	name        string
 	age, gender int
 	// 匿名字段
 	float64
+}
+
+// 不可导出的结构体
+type project struct {
+	Path string
+}
+
+func NewProject(path string) project {
+	return project{Path: path}
+}
+
+func (p *project) Build(out string) {
+	fmt.Printf("Build() project '%s' to '%s'\n", p.Path, out)
 }
 
 func structRun() {
@@ -22,7 +35,7 @@ func structRun() {
 		age:    22,
 		gender: 1,
 	}
-	tom.float64 = 5000.09
+	tom.float64 = 5000.0
 	fmt.Println("tom is", tom)
 
 	// 匿名结构体
@@ -41,9 +54,12 @@ func structRun() {
 
 	// 结构体指针
 	mary := &Person{
-		name: "mary",
-		age: 18,
+		name:   "mary",
+		age:    18,
 		gender: 0,
 	}
 	fmt.Println("mary.age is", mary.age)
+
+	p := NewProject("sample-go")
+	p.Build(p.Path + "/build")
 }
