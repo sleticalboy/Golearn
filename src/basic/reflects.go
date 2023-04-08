4279d322bc9b039a27f1504f31ebac7bc02fcf6a
sleticalboy <sleticalboy@gmail.com>
2023-04-08 23:14:25 +0800

    feat: reflect struct methods

--- src/basic/reflects.go
+++ src/basic/reflects.go
@@ -5,27 +5,41 @@ import (
 	"reflect"
 )
 
-type nested struct {
-	cc string
+type ISay interface {
+	Say()
+}
+
+type Nested struct {
+	Cc string
+}
+
+func (n Nested) Say() {
+	fmt.Println("sample.Say() run...")
+}
+
+func (n Nested) String() string {
+	return fmt.Sprintf("Nestedt{Cc: %s}", n.Cc)
+}
+
+func (n Nested) What() (a, b, c int, reason string) {
+	return 1, 2, 3, "nil"
 }
 
 type foo struct {
 	a string
 	b int
-	c nested
+	n Nested
 }
 
 type sample struct {
 	name    string
 	age     int
-	sallery float32
+	salary  float32
 	subject []string
 	fo      foo
 }
 
-func (s *sample) Say() {}
-
-func ident(depth int) {
+func indent(depth int) {
 	for i := 0; i < depth; i++ {
 		fmt.Printf("  ")
 	}
@@ -44,7 +58,7 @@ func printValue(vf reflect.Value, depth int) {
 		fmt.Printf(" -> ")
 		slice := vf.Slice(0, len)
 		for i := 0; i < len; i++ {
-			fmt.Printf("[%d]: %s", i, slice.Index(i).String())
+			fmt.Printf("[%d]: %v", i, slice.Index(i))
 			if i != len-1 {
 				fmt.Printf(", ")
 			} else {
@@ -53,37 +67,65 @@ func printValue(vf reflect.Value, depth int) {
 		}
 	case reflect.Struct:
 		fmt.Printf(" <struct>:\n")
-		ref(vf, depth+1)
+		refWithDepth(vf, depth+1)
 	default:
 		fmt.Printf(" -> %v\n", vf)
 	}
 }
 
 func refInternal(t reflect.Type, v reflect.Value, depth int) {
-	switch t.Kind() {
+	switch k := t.Kind(); k {
 	case reflect.Struct:
-		n := t.NumField()
-		// ident(depth)
-		// fmt.Printf("type: %v, methods: %d, fields: %d\n", t, t.NumMethod(), n)
-		for i := 0; i < n; i++ {
-			ident(depth)
-			tf := t.Field(i)
-			fmt.Printf("%s.%s[%v]", t.Name(), tf.Name, tf.Type)
-			printValue(v.Field(i), depth)
+		n := 0
+		if n := t.NumField(); n > 0 {
+			indent(depth)
+			fmt.Printf("type: %v, methods: %d, fields: %d\n", t, t.NumMethod(), n)
+			indent(depth)
+			fmt.Printf("struct fields >>>>>>\n")
+			for i := 0; i < n; i++ {
+				indent(depth)
+				tf := t.Field(i)
+				fmt.Printf("%s.%s[%v]", t.Name(), tf.Name, tf.Type)
+				printValue(v.Field(i), depth)
+			}
+		}
+		// 值函数才能反射出来？
+		if n = t.NumMethod(); n > 0 {
+			indent(depth)
+			fmt.Printf("struct methods >>>>>>\n")
+			for i := 0; i < n; i++ {
+				indent(depth)
+				tm := t.Method(i)
+				if no := tm.Type.NumOut(); no > 0 {
+					fmt.Printf("%s.%s()", t.Name(), tm.Name)
+					for j := 0; j < no; j++ {
+						fmt.Printf(" %s", tm.Type.Out(j).String())
+					}
+					fmt.Printf("\n")
+				} else {
+					fmt.Printf("%s.%s()\n", t.Name(), tm.Name)
+				}
+			}
 		}
 	case reflect.Func:
-		fmt.Printf("func name is '%v'\n", v)
+		fmt.Printf("sig(%s)\n", t.String())
 		fmt.Printf("t.NumIn(): %v\n", t.NumIn())
 		fmt.Printf("t.NumOut(): %v\n", t.NumOut())
 		fmt.Printf("t.In(0): %v\n", t.In(0))
 		fmt.Printf("t.Out(0): %v\n", t.Out(0))
+	case reflect.Interface:
+		fmt.Printf("%s is a interface\n", t.Name())
 	default:
 		fmt.Printf("%s[%v]", t.Name(), v.Type())
 		printValue(v, depth)
 	}
 }
 
-func ref(o any, depth int) {
+func ref(o any) {
+	refWithDepth(o, 0)
+}
+
+func refWithDepth(o any, depth int) {
 	if o == nil {
 		fmt.Println("nil")
 		return
@@ -96,38 +138,33 @@ func ref(o any, depth int) {
 	}
 }
 
-func fooo(i int) string {
-	return "hello foo"
-}
-
 func reflectsRun() {
 	println("\nreflects run..")
 
 	s := sample{
 		name:    "tom",
 		age:     24,
-		sallery: 9000.0,
+		salary:  9000.0,
 		subject: []string{"java", "go", "c++"},
 		fo: foo{
 			a: "aaa",
 			b: 111,
-			c: nested{
-				cc: "nested struct depth 2",
+			n: Nested{
+				Cc: "nested struct depth 2",
 			},
 		},
 	}
-	ref(s, 0)
+	ref(s)
 
-	ref("hello", 0)
-	ref(float32(9), 0)
-	ref(float64(88), 0)
-	ref(nil, 0)
-	ref(true, 0)
-	ref(2, 0)
-	ref(int32(23), 0)
-
-	// ano := func(a int) string {
-	// 	return "sdkkd"
-	// }
-	ref(fooo, 0)
+	ref("hello")
+	ref(float32(9))
+	ref(float64(88))
+	ref(nil)
+	ref(true)
+	ref(2)
+	ref(int32(23))
+	ref(func(a int) string {
+		return "sdkkd"
+	})
+	ref([]string{"1", "2"})
 }
