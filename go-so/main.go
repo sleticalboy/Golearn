7a991ee96be7246f159e6b9016726a470f780ddd
bin.li <sleticalboy@gmail.com>
2023-10-30 14:49:52 +0800

    test: go uint64 overflow

--- go-so/main.go
+++ go-so/main.go
@@ -1,8 +1,22 @@
 package main
 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct { char *name; int age; char *home; } Person;
+typedef unsigned long long GoUint64_;
+*/
 import "C"
-import "fmt"
+import (
+	"fmt"
+)
 import "time"
+
+//export HelloGo
+func HelloGo(s *C.char) {
+	fmt.Printf("HelloGo, %v!\n", C.GoString(s))
+}
 
 //export GoFib
 func GoFib() {
@@ -17,12 +31,39 @@ func GoFib() {
 }
 
 //export Fib
-func Fib(value int) int {
+func Fib(value int) uint64 {
 	// fmt.Printf("Fib() in go, value: %d", value)
-	if value == 1 || value == 2 {
-		return value
+	if value < 2 {
+		return 1
 	}
-	return Fib(value-1) + Fib(value-2)
+
+	var f = uint64(1)
+	var s = uint64(1)
+	var temp uint64
+	for i := 0; i < value; i++ {
+		if i < 2 {
+			continue
+		}
+		temp = s
+		s = s + f
+		if i > 90 {
+			fmt.Printf("Fib() %d -> %d\n", i, s)
+		}
+		f = temp
+	}
+	return s
 }
 
-func main() {}
+//export GoHome
+func GoHome(ming *C.Person) {
+	fmt.Printf("GoHome() -> name: %v, age: %v, home: %v\n", C.GoString(ming.name), ming.age, C.GoString(ming.home))
+}
+
+//export GoHome2
+func GoHome2(ming C.Person) {
+	fmt.Printf("GoHome2() -> name: %v, age: %v, home: %v\n", C.GoString(ming.name), ming.age, C.GoString(ming.home))
+}
+
+func main() {
+	fmt.Println(Fib(93))
+}
--- go-so/test.py
+++ go-so/test.py
@@ -2,21 +2,50 @@ import ctypes
 import time
 
 so = ctypes.cdll.LoadLibrary('./libfib.so')
-i_ = 10
+
+so.HelloGo(b"Python 3")
+
+i_ = 93
 
 start = time.time()
 r = so.Fib(i_)
 end = time.time()
-print(f'go fib {i_} -> {r}, cost: {end - start}s')
+print(f'go fib {i_} -> {r}, type: {type(r)}, cost: {end - start}s')
 
 
 def fib(value: int) -> int:
-    if value == 1 or value == 2:
-        return value
-    return fib(value - 1) + fib(value - 2)
+    if value < 2:
+        return 1
+    f = 1
+    s = 1
+    for i in range(value):
+        if i < 2:
+            continue
+        temp = s
+        s = f + s
+        if i > 90:
+            print(f"{i} -> {s}")
+        f = temp
+    return s
 
 
 start = time.time()
 r = fib(i_)
 end = time.time()
-print(f'python fib {i_} -> {r}, cost: {end - start}s')
+print(f'python fib {i_} -> {r}, type: {type(r)}, cost: {end - start}s')
+
+
+class PyPerson(ctypes.Structure):
+    _fields_ = [
+        ('name', ctypes.c_char_p),
+        ('age', ctypes.c_int),
+        ('home', ctypes.c_char_p),
+    ]
+
+
+go_home = so.GoHome
+p = PyPerson(b'li ming', 28, b'chain henan')
+# 指针类型
+so.GoHome(ctypes.byref(p))
+# 非指针类型
+so.GoHome2(p)
